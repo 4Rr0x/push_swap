3edeb265a3cd8c3ab6a8a0fe26a3a95222180827
4Rr0x <joaopfarreca@gmail.com>
2025-01-20 16:35:34 +0000

    makefile and header

---
+++ push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jopedro- <jopedro-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 16:03:15 by jopedro-          #+#    #+#             */
+/*   Updated: 2025/01/20 16:35:13 by jopedro-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "lib/libft/libft/libft.h"
+# include "../lib/libft/ft_printf/ft_printf.h"
+
+
+
+#endif
